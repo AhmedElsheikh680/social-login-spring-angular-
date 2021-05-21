0bf79665a65c568f3be4551d7475755d881bbc4d
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-05-21 16:30:18 +0200

    login and logout

--- front-end/src/app/components/social/social.component.ts
+++ front-end/src/app/components/social/social.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FacebookLoginProvider, GoogleLoginProvider, SocialAuthService} from 'angularx-social-login';
+import {FacebookLoginProvider, GoogleLoginProvider, SocialAuthService, SocialUser} from 'angularx-social-login';
 
 @Component({
   selector: 'app-social',
@@ -8,13 +8,21 @@ import {FacebookLoginProvider, GoogleLoginProvider, SocialAuthService} from 'ang
 })
 export class SocialComponent implements OnInit {
 
+  socialUser: SocialUser;
+  isLogin: boolean; // default false
   constructor(private socialAuthService: SocialAuthService) { }
 
   ngOnInit(): void {
+    this.socialAuthService.authState.subscribe(
+      data => {
+        this.isLogin = (data !=null);
+      }
+    );
   }
   signInWithGoogle(): void {
     this.socialAuthService.signIn(GoogleLoginProvider.PROVIDER_ID).then(
       data => {
+        this.socialUser = data;
         console.log(data);
       }
     );
@@ -23,6 +31,7 @@ export class SocialComponent implements OnInit {
   signInWithFB(): void {
     this.socialAuthService.signIn(FacebookLoginProvider.PROVIDER_ID).then(
       data => {
+        this.socialUser = data;
         console.log(data);
       }
     );

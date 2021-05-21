14c30020d991a90d6f3a82457e71b23732f47b59
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2021-05-21 18:46:37 +0200

    angular show data of user

--- front-end/src/app/components/header/header.component.ts
+++ front-end/src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {SocialAuthService, SocialUser} from 'angularx-social-login';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  socialUser: SocialUser;
+  isLogin: boolean;
+  constructor(private socialAuthService: SocialAuthService) { }
 
   ngOnInit(): void {
+    this.socialAuthService.authState.subscribe(
+      data => {
+        this.isLogin = (data != null);
+        this.socialUser = data;
+      }
+    );
   }
 
 }
